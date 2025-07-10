8917b28ff95ec95ccb3636fa64a3ef3d68580245
Habeeba Khatoon <Khabeeba18@gmail.com>
2025-07-11 01:51:55 +0530

    Day 7

--- Hashing/HashBasic.java
+++ Hashing/HashBasic.java
@@ -25,11 +25,34 @@ public class HashBasic {
         Map<Integer ,Integer> m = new HashMap<>();
         for(int i = 0; i < n;i++){
             if(m.containsKey(a[i])){
-                m.put(a[i], m.get(a[i]) + 1);
+                m.put(a[i], m.get(a[i]) + 1);    //m.put(a[i] ,m.getOrDefault(a[i],0) +1);
             } else {
                 m.put(a[i], 1);
             }
         }
         System.out.println(m.get(key)); // O(1) time complexity
+
+
+
+        //Finding frequency of highest and lowest frequency elements
+        int maxF = 0;
+        int minF = n;
+        int maxEl = 0, minEl = 0;
+        for(Map.Entry<Integer,Integer> entry : m.entrySet()){
+            int f = entry.getValue();
+            int el = entry.getKey();
+            if(f > maxF){
+                maxF = f;
+                maxEl = el;
+            }
+            if(f < minF){
+                minEl = el;
+                minF = f;
+            }
+        }
+        System.out.println("Max Element " + maxEl + ":" + maxF);
+        System.out.println("Min Element " + minEl + ":" + minF);
+
+       
     }
 }
